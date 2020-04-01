966eb2c56e586113681db885cc3262abb61a15d5
barbara-mooney <60058920+barbara-mooney@users.noreply.github.com>
2020-04-01 10:00:17 -0700

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// write your code here
+const http = require('http');
+
+const server = http.createServer(function(req, res) {
+  console.log('Received' + req.method + 'request for:' + req.url);
+  res.writeHead(200, {'Content-type': 'text/plain'});
+  res.end('Hello World');
+});
+server.listen(8080, 'localhost', null, function() {
+  console.log('Server is listening in port: 8080');
+});
+
+ERROR im getting
+hello-express
+    1) "before each" hook for "should have the correct page title"
+
+
+  0 passing (3ms)
+  1 failing
+
+  1) hello-express "before each" hook for "should have the correct page title":
+     ReferenceError: Nightmare is not defined
+      at Context.<anonymous> (test\main.spec.js:13:5)
+      at processImmediate (internal/timers.js:439:21)
+
+
--- test/main.spec.js
+++ test/main.spec.js
@@ -1,4 +1,4 @@
-var Nightmare = require('nightmare');
+var nightmare = require('nightmare');
 var expect = require('chai').expect;
 var axios = require('axios');
 
@@ -8,11 +8,11 @@ const url = 'http://localhost:8080';
 describe('hello-express', function () {
   this.timeout(6500);
   this.slow(3000);
-  
+
   beforeEach(()=> {
     nightmare = Nightmare();
   })
-  
+
   it('should have the correct page title', () =>
     nightmare
       .goto(url)
@@ -24,7 +24,7 @@ describe('hello-express', function () {
         expect(text).to.equal('Hello World');
       })
   );
-  
+
   it('returns the correct status code', () => axios.get('http://localhost:8080')
     .then((response) => {
         return expect(response.status === 200)
